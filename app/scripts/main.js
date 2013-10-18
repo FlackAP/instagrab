4be14ca2ba8d46ec04c62226f61473ccfd350142
FlackAP <flack.ap@gmail.com>
2013-10-18 19:02:28 -0400

    further work on nav bar positioning, styling, and functionality

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,37 @@
-	
-
 $('#tags').tagsInput({
 });
+
+
+$('.start').click(function(){
+	$('.nav').addClass('nav-full')
+	$('.tagsinput').css('opacity', "1")
+	$('.hide-button').css('opacity', "1")
+	$('.start').css('opacity', '0')
+	$('.nav').css('height', '200px')
+})
+
+$('.hide-button').click(function(){
+	$('.nav').css('height', '120px')
+	$('.tagsinput').css('opacity', '0')
+	$('.start').css('opacity', '1')
+	$('.hide-button').css('opacity', "0")
+})
+
+$(function() {
+    $( "#from" ).datepicker({
+      defaultDate: "+1w",
+      changeMonth: true,
+      numberOfMonths: 3,
+      onClose: function( selectedDate ) {
+        $( "#to" ).datepicker( "option", "minDate", selectedDate );
+      }
+    });
+    $( "#to" ).datepicker({
+      defaultDate: "+1w",
+      changeMonth: true,
+      numberOfMonths: 3,
+      onClose: function( selectedDate ) {
+        $( "#from" ).datepicker( "option", "maxDate", selectedDate );
+      }
+    });
+  });
